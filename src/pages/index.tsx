16b82ac1db73b3689a7363560ad13bdbdb47ba80
Wander Aquino <wander.aquino@gmail.com>
2021-08-04 01:14:57 -0300

    Tracking initial frame index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
-import { GetStaticProps } from 'next';
-
-import { getPrismicClient } from '../services/prismic';
-
-import commonStyles from '../styles/common.module.scss';
+import Head from "next/head";
 import styles from './home.module.scss';
+import {MdDateRange} from "react-icons/md";
+import {FiUser} from "react-icons/fi";
+
+import { GetStaticProps } from 'next';
+import { getPrismicClient } from '../services/prismic';
+import commonStyles from '../styles/common.module.scss';
 
 interface Post {
   uid?: string;
@@ -24,9 +26,28 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | SpaceTravelling</title>
+      </Head>
+      <main>
+        <div className={styles.postContainer}>
+          <div className={styles.post}>
+            <span>Como utilizar hooks</span>
+            <div className={styles.postFooter}>
+              <MdDateRange />
+              <span>15 Mar 2021</span>
+              <FiUser />
+              <span>Danilo Vieira</span>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
