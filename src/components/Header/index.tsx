07b25ad2eef1eb000b94e0cb67581f24fcc03a4c
Wander Aquino <wander.aquino@gmail.com>
2021-08-04 01:13:38 -0300

    Tracking header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
+import styles from "./header.module.scss";
+
 export default function Header() {
-  // TODO
+  return (
+    <>
+      <header className={styles.headerContainer}>
+        <div className={styles.headerContent}>
+          <img src="./images/logo.svg"></img>
+        </div>
+      </header>
+    </>
+  )
 }
