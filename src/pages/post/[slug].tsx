c59667775d75d9b31d1344fc8b5e9112f6fc21c1
Wander Aquino <wander.aquino@gmail.com>
2021-08-04 23:25:47 -0300

    Starting provisional static post page

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -4,6 +4,8 @@ import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import {MdDateRange} from "react-icons/md";
+import {FiUser} from "react-icons/fi";
 
 interface Post {
   first_publication_date: string | null;
@@ -26,9 +28,39 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post() {
+  return (
+    <>
+    <article className={styles.postBanner}>
+      <img src="/images/banner.png"></img>
+    </article>
+
+    <article className={styles.postContent}>
+      <div className={styles.postHeading}>
+        <h1>Criando um App CRA do Zero</h1>
+
+        <span className={styles.uploadedAt}>
+              <MdDateRange color="#BBBBBB" size="20px"/>
+              01 Ago 2021
+            </span>
+
+        <span className={styles.userInfo}>
+          <FiUser color="#BBBBBB" size="20px"/>
+          Danilo Vieira
+        </span>
+
+      </div>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Ex assumenda ullam nam nobis cupiditate eum dolorem, 
+        repellat quasi, cum expedita modi deserunt omnis totam reprehenderit dolore? 
+        Eum unde ea odio?
+      </p>
+    </article>
+    </>
+  )
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient();
