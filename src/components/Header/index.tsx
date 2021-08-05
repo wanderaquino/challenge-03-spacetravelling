739305a607e4a4b38d3556d08c9a114b7675d916
Wander Aquino <wander.aquino@gmail.com>
2021-08-04 23:25:00 -0300

    Fixing image import

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <>
       <header className={styles.headerContainer}>
         <div className={styles.headerContent}>
-          <img src="./images/logo.svg"></img>
+          <img src="/images/logo.svg" alt="SpaceTraveling logo"></img>
         </div>
       </header>
     </>
