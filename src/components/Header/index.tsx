bac72659ec2d542d1fa895d621268201d9e09db1
Wander Aquino <wander.aquino@gmail.com>
2021-08-10 22:33:04 -0300

    INcluding Link next component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,15 @@
 import styles from "./header.module.scss";
+import Link from "next/link"
 
 export default function Header() {
   return (
     <>
       <header className={styles.headerContainer}>
-        <a href="/" className={styles.headerContent}>
-          <img src="/images/logo.svg" alt="logo"></img>
-        </a>
+        <Link href="/">
+            <a className={styles.headerContent}>
+            <img src="/images/logo.svg" alt="logo"></img>
+            </a>
+        </Link>
       </header>
     </>
   )
