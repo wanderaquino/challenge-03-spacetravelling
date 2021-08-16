a58c51fe994a7f9195a9f952f25c94e7b2290ba4
Wander Aquino <wander.aquino@gmail.com>
2021-08-16 12:26:41 -0300

    ADding accessToken for preview feature

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -7,6 +7,7 @@ import { PromiseCompletionFunction } from 'yargs';
 export function getPrismicClient(req?: unknown): DefaultClient {
   const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT, {
     req,
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN
   });
 
   return prismic;
