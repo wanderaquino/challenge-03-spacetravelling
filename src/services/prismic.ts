5b1028d0a092f9cf543dc11b73ea88af643cccba
Wander Aquino <wander.aquino@gmail.com>
2021-08-16 00:30:51 -0300

    Including linkResolver functionm

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,5 +1,8 @@
 import Prismic from '@prismicio/client';
 import { DefaultClient } from '@prismicio/client/types/client';
+import { Document } from '@prismicio/client/types/documents';
+import PrismicDOM from "prismic-dom"
+import { PromiseCompletionFunction } from 'yargs';
 
 export function getPrismicClient(req?: unknown): DefaultClient {
   const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT, {
@@ -7,4 +10,12 @@ export function getPrismicClient(req?: unknown): DefaultClient {
   });
 
   return prismic;
+}
+
+export function linkResolver (document: Document) {
+  if(document.type === "post") {
+    return `/post/${document.uid}`;
+  }
+
+  return "/";
 }
