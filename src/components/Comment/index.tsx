1be5bdb861bcae90221ba1d406f0955e1b7dbbd6
Wander Aquino <wander.aquino@gmail.com>
2021-08-16 22:25:05 -0300

    Fixing avoiding double comment render component

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -5,15 +5,17 @@ export default function Comment() {
       if (!elem) {
         return;
       }
-      const scriptElem = document.createElement("script");
-      scriptElem.src = "https://utteranc.es/client.js";
-      scriptElem.async = true;
-      scriptElem.crossOrigin = "anonymous";
-      scriptElem.setAttribute("repo", "wanderaquino/challenge-03-spacetravelling");
-      scriptElem.setAttribute("issue-term", "pathname");
-      scriptElem.setAttribute("label", "post-comment");
-      scriptElem.setAttribute("theme", "github-dark");
-      elem.appendChild(scriptElem);
+      if(!document.querySelector(".utterances")){
+        const scriptElem = document.createElement("script");
+        scriptElem.src = "https://utteranc.es/client.js";
+        scriptElem.async = true;
+        scriptElem.crossOrigin = "anonymous";
+        scriptElem.setAttribute("repo", "wanderaquino/challenge-03-spacetravelling");
+        scriptElem.setAttribute("issue-term", "pathname");
+        scriptElem.setAttribute("label", "post-comment");
+        scriptElem.setAttribute("theme", "github-dark");
+        elem.appendChild(scriptElem);
+      }
     }}
   />)   
 }
